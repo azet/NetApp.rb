13c318c620b2618fbf98cb78529be6b18e29fc8a
Aaron Zauner <azet@azet.org>
2013-05-13 17:25:21 +0200

    added examples

---
+++ examples/example_REST_00.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+
+# require shit
+require 'rubygems'
+require 'sinatra'
+require 'json'
+#require 'bcrypt'
+require './lib/netapp.rb'
+
+# credentials and shit
+@@fileraddr = "192.168.55.10"
+@@fileruser = "root"
+@@filerpwd = "rootr00t"
+
+@@authuser = "admin"
+@@authpwd = "testpwd"
+
+
+# filer stanza shit
+def conn_filer
+	# implement something generic over here
+	Filer.new(@@fileraddr, @@fileruser, @@filerpwd)
+end
+
+# authorize shit
+helpers do  
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [@@authuser, @@authpwd]
+  end
+end
+
+# parse shit
+get '/' do
+	"nope."
+end
+
+get '/aggregate' do
+	protected!
+	conn_filer
+	body(Aggregate.list).to_json
+end
+
+get '/volume' do
+	protected!
+	conn_filer
+	body(Volume.list).to_json
+end
+
+put '/volume' do
+	protected!
+	conn_filer
+	status 201 if Volume.create(params[:aggregate], params[:volumename], params[:size])
+end
+
+delete '/volume' do
+	protected!
+	conn_filer
+	status 200 if Volume.offline(params[:volumename]) and Volume.purge(params[:volumename])
+end
+
+get '/quota' do
+	protected!
+	conn_filer
+	body(Quota.list).to_json
+end
+
+put '/quota' do
+	protected!
+	conn_filer
+	volumepath = "/vol/${params[:volumename}/"
+	status 201 if Quota.create(params[:volumename], volumepath, params[:quotasize], params[:type])
+end
+
+delete '/quota' do
+	protected!
+	conn_filer
+	status 200 if Quota.purge(params[:quotaname], params[:volumename], params[:path], params[:type])
+end
+
